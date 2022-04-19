df7658f7df9fd131c59c5843dcfd84dd03f89f16
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2022-04-19 17:04:17 +0300

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,35 @@
+from time import sleep
+from threading import Thread
+from pynput import keyboard
+import pyautogui
+import random
+
+ekranX, ekranY = pyautogui.size()
+x = int(ekranX/2)
+y = int(ekranY/2)
+
+def on_press(key, abortKey='esc'):    
+    try:
+        k = key.char 
+    except:
+        k = key.name
+    if k == abortKey:
+        print('end loop ...')
+        return False
+
+def dongu():
+    while True:
+        sleep(random.randint(500, 600))
+        pyautogui.moveTo(x, y-100)
+        sleep(1)
+        pyautogui.click()
+        sleep(random.randint(500, 600))
+        pyautogui.moveTo(x, y+100)
+        sleep(1)
+        pyautogui.click()
+
+if __name__ == '__main__':
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start() 
+    Thread(target=dongu, args=(), name='dongu', daemon=True).start()
+    listener.join()
